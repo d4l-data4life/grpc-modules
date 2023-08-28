1dcac9f0108eba4c46b00972e3f219ada775c5da
LRueckert <lars.rueckert@data4life.care>
2023-08-28 16:34:39 +0200

    ✨ GRPC server to server utils

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - research-pillars
+- utils for grpc server-to-server communication
 
 ### Changed
 
---
+++ pkg/utils/connection/connection.go
@@ -0,0 +1,32 @@
+package connection
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/gesundheitscloud/go-svc/pkg/logging"
+	"github.com/gesundheitscloud/grpc-modules/pkg/utils/auth"
+)
+
+func OpenWithSecretAuth(runCtx context.Context, url, secret string) (*grpc.ClientConn, error) {
+	secretAuth := auth.SecretAuthCreds{Secret: secret}
+	grpcConn, err := grpc.Dial(
+		url,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithPerRPCCredentials(secretAuth),
+	)
+	if err != nil {
+		logging.LogErrorfCtx(runCtx, err, "could not establish grpc connection to %s", url)
+		return nil, err
+	}
+
+	// Wait for context to be done to close connection
+	go func() {
+		<-runCtx.Done()
+		grpcConn.Close()
+	}()
+
+	return grpcConn, nil
+}
